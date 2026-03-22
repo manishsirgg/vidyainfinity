01f8b3b341b66c6de9afa2b59aa06651635804c1
manishsirgg <manishsirgg@gmail.com>
2026-03-22 21:02:36 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import OneSignal from 'react-onesignal';
-
-async function initOneSignal() {
-  await OneSignal.init({
-    appId: "fc73e175-c863-4d14-ae52-6876766384d7",
-    notifyButton: {
-      enable: false,
-    },
-    allowLocalhostAsSecureOrigin: true
-  });
-}
-
-initOneSignal();
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");

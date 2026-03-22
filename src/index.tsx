1e7e563472f8a81be4c56df8ec348b1663d7b264
manishsirgg <manishsirgg@gmail.com>
2026-03-22 20:05:04 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';  
+import './index.css';
+
+import OneSignal from 'react-onesignal';
+
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: "fc73e175-c863-4d14-ae52-6876766384d7",
+    notifyButton: {
+      enable: false,
+    },
+    allowLocalhostAsSecureOrigin: true
+  });
+}
+
+initOneSignal();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
